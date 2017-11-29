2b51e38055d8cd168722f8b62a07cba130da3185
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-11-29 11:25:45 +0530

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,6 +1,6 @@
 <?php
 
-class Smashing_Updater {
+class Gitlab_Updater {
 
 	private $file;
 
@@ -18,6 +18,8 @@ class Smashing_Updater {
 
 	private $github_response;
 
+        private $ap_url;
+        
 	public function __construct( $file ) {
 
 		$this->file = $file;
@@ -28,6 +30,7 @@ class Smashing_Updater {
 	}
 
 	public function set_plugin_properties() {
+       
 		$this->plugin	= get_plugin_data( $this->file );
 		$this->basename = plugin_basename( $this->file );
 		$this->active	= is_plugin_active( $this->basename );
@@ -38,66 +41,53 @@ class Smashing_Updater {
 	}
 
 	public function set_repository( $repository ) {
+
 		$this->repository = $repository;
 	}
 
 	public function authorize( $token ) {
 		$this->authorize_token = $token;
 	}
+        public function api($url) {
+        $this->ap_url = $url;
+    }
 
-	private function get_repository_info() {
-	    if ( is_null( $this->github_response ) ) { // Do we have a response?
-	        $request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository ); // Build URI
+    private function get_repository_info() {
+        if (is_null($this->github_response)) { // Do we have a response?
+            $queryParams['private_token'] = $this->authorize_token;
+          //  http://172.161.0.102/api/v4/projects/4/repository/tags
+            $url = add_query_arg($queryParams, $this->ap_url.'/api/v4/projects/4/repository/tags');
+            $options = array('timeout' => 10);
+            $response = json_decode(wp_remote_retrieve_body(wp_remote_get($url)), true);
+            $this->github_response = $response[0]; // Set it to our property			 
+        }
+    }
 
-	        if( $this->authorize_token ) { // Is there an access token?
-	            $request_uri = add_query_arg( 'access_token', $this->authorize_token, $request_uri ); // Append it
-	        }
-
-	        $response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri ) ), true ); // Get JSON and parse it
-
-	        if( is_array( $response ) ) { // If it is an array
-	            $response = current( $response ); // Get the first item
-	        }
-
-	        if( $this->authorize_token ) { // Is there an access token?
-	            $response['zipball_url'] = add_query_arg( 'access_token', $this->authorize_token, $response['zipball_url'] ); // Update our zip url with token
-	        }
-
-	        $this->github_response = $response; // Set it to our property
-	    }
-	}
-
-	public function initialize() {
+    public function initialize() {
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'modify_transient' ), 10, 1 );
 		add_filter( 'plugins_api', array( $this, 'plugin_popup' ), 10, 3);
 		add_filter( 'upgrader_post_install', array( $this, 'after_install' ), 10, 3 );
 	}
 
 	public function modify_transient( $transient ) {
+            
+		if( $checked = $transient->checked ) { // Did Wordpress check for updates?
 
-		if( property_exists( $transient, 'checked') ) { // Check if transient has a checked property
+			$this->get_repository_info(); // Get the repo info
+			$out_of_date = version_compare( ltrim($this->github_response['name'],'v'), $checked[ $this->basename ] ); // Check if we're out of date
+			if( $out_of_date ) {
+    $latest_version = $this->github_response['name'];
+///$plugin_package = "http://172.161.0.102/api/v4/projects/4/repository/archive.zip?sha=$latest_version&private_token=DsknQsX81pWQD342WK1H";
 
-			if( $checked = $transient->checked ) { // Did Wordpress check for updates?
+$new_files = $this->ap_url.'/api/v4/projects/4/repository/archive.zip?sha='.$latest_version.'&private_token='.$this->authorize_token.''; 
+				$plugin = array( // setup our plugin info
+					'url' => $this->plugin["PluginURI"],
+					'slug' => $this->basename,
+					'package' => $new_files,
+					'new_version' => $this->github_response['name']
+				);
 
-				$this->get_repository_info(); // Get the repo info
-
-				$out_of_date = version_compare( $this->github_response['tag_name'], $checked[ $this->basename ], 'gt' ); // Check if we're out of date
-
-				if( $out_of_date ) {
-
-					$new_files = $this->github_response['zipball_url']; // Get the ZIP
-
-					$slug = current( explode('/', $this->basename ) ); // Create valid slug
-
-					$plugin = array( // setup our plugin info
-						'url' => $this->plugin["PluginURI"],
-						'slug' => $slug,
-						'package' => $new_files,
-						'new_version' => $this->github_response['tag_name']
-					);
-
-					$transient->response[$this->basename] = (object) $plugin; // Return it in response
-				}
+				$transient->response[$this->basename] = (object) $plugin; // Return it in response
 			}
 		}
 
@@ -107,8 +97,8 @@ class Smashing_Updater {
 	public function plugin_popup( $result, $action, $args ) {
 
 		if( ! empty( $args->slug ) ) { // If there is a slug
-			
-			if( $args->slug == current( explode( '/' , $this->basename ) ) ) { // And it's our slug
+
+			if( $args->slug == $this->basename ) { // And it's our slug
 
 				$this->get_repository_info(); // Get our repo info
 
@@ -116,19 +106,13 @@ class Smashing_Updater {
 				$plugin = array(
 					'name'				=> $this->plugin["Name"],
 					'slug'				=> $this->basename,
-					'requires'					=> '3.3',
-					'tested'						=> '4.4.1',
-					'rating'						=> '100.0',
-					'num_ratings'				=> '10823',
-					'downloaded'				=> '14249',
-					'added'							=> '2016-01-05',
 					'version'			=> $this->github_response['tag_name'],
 					'author'			=> $this->plugin["AuthorName"],
 					'author_profile'	=> $this->plugin["AuthorURI"],
 					'last_updated'		=> $this->github_response['published_at'],
 					'homepage'			=> $this->plugin["PluginURI"],
 					'short_description' => $this->plugin["Description"],
-					'sections'			=> array(
+					'sections'			=> array( 
 						'Description'	=> $this->plugin["Description"],
 						'Updates'		=> $this->github_response['body'],
 					),
@@ -138,14 +122,14 @@ class Smashing_Updater {
 				return (object) $plugin; // Return the data
 			}
 
-		}
+		}	
 		return $result; // Otherwise return default
 	}
 
 	public function after_install( $response, $hook_extra, $result ) {
 		global $wp_filesystem; // Get global FS object
 
-		$install_directory = plugin_dir_path( $this->file ); // Our plugin directory
+		$install_directory = plugin_dir_path( $this->file ); // Our plugin directory 
 		$wp_filesystem->move( $result['destination'], $install_directory ); // Move files to the plugin dir
 		$result['destination'] = $install_directory; // Set the destination for the rest of the stack
 
