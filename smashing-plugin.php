2d70f19ba71c1b6e76716bad20b3418d56828f53
ullakalaim8 <kaleemon.mohammed@gmail.com>
2017-11-29 11:25:30 +0530

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -1,19 +1,18 @@
-<?php
+<?php 
 /*
-	Plugin Name: Smashing Plugin
+	Plugin Name: Gitlab
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.0
-	Author: Matthew Ray
-	Author URI: http://www.matthewray.com
+	Version: 8.0.6
+	Author: Mohammed kalimulla
+	Author URI: http://www.lsnsoft.com/
 */
-if( ! class_exists( 'Smashing_Updater' ) ){
-	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
-}
 
-$updater = new Smashing_Updater( __FILE__ );
-$updater->set_username( 'rayman813' );
-$updater->set_repository( 'smashing-updater-plugin' );
-/*
-	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
-*/
+include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+
+$updater = new Gitlab_Updater( __FILE__ );
+//$updater->set_username( 'rayman813' );
+$updater->set_repository( 'gitlab-test' );
+$updater->api('http://172.161.0.102');
+$updater->set_repository(4);
+$updater->authorize( 'DsknQsX81pWQD342WK1H' ); // Your auth code goes here for private repos
 $updater->initialize();
